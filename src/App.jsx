6245c175bb610ad80f4fdedc24dc3059bd770679
Uchenna <ansonuchenna@gmail.com>
2023-11-06 07:53:46 +0000

    Setting up local storage and adding react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import { useState } from "react";
 import Form from './Form';
 import  {nanoid } from 'nanoid';
 import Items from './Items';
+import { ToastContainer, toast } from "react-toastify";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if(list) {
+    list = JSON.parse(localStorage.getItem('list'))
+  } else {
+    list = []
+  }
+  return list
+}
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items))
+};
+
 
 const App = () => {
-  const [items, setItems] = useState ([])
+  const [items, setItems] = useState (getLocalStorage())
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,18 +28,36 @@ const App = () => {
       completed: false,
       id: nanoid()
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem]
+    setItems(newItems);
+    setLocalStorage(newItems)
+    toast.success('item added to the list');
   }
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems)
+     toast.error('item deleted');
+  }
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if(item.id === itemId) {
+        const newItem ={...item, completed:!item.completed}
+        return newItem;
+      }
+      return item
+    })
+    setItems(newItems);
+    setLocalStorage(newItems);
   }
 
   return (
     <section className='section-center'>
+      <ToastContainer position= 'top-center' />
     <Form addItem={addItem} />
-    <Items items={items} removeItem = {removeItem} />
+    <Items items={items} removeItem = {removeItem}  editItem ={editItem}/>
   </section>
   );
 };
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import {  toast } from 'react-toastify';
 
 const Form = ({ addItem }) => {
     const [newItemName, setNewItemName] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if(!newItemName) return;
+        if(!newItemName) {
+            toast.error('Please provide a value')
+            return};
         addItem(newItemName)
         setNewItemName('');
     }
