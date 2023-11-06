b296d3acf4864ba3ba8de4cb190fac7f08e1e770
Uchenna <ansonuchenna@gmail.com>
2023-11-06 07:05:05 +0000

    Building of Grocery Building

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Form from './Form';
+import  {nanoid } from 'nanoid';
+import Items from './Items';
+
+const App = () => {
+  const [items, setItems] = useState ([])
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid()
+    };
+    setItems([...items, newItem]);
+  }
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+  }
+
+  return (
+    <section className='section-center'>
+    <Form addItem={addItem} />
+    <Items items={items} removeItem = {removeItem} />
+  </section>
+  );
+};
+
+export default App;
